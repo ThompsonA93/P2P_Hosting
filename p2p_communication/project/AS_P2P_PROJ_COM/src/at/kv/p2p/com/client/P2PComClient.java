a1551b3a7ebe78cb710a35729ac3240f7028c291
gitolafson <35873933+gitolafson@users.noreply.github.com>
2018-06-01 20:47:00 +0200

    Peer communication implemented

---
+++ p2p_communication/project/AS_P2P_PROJ_COM/src/at/kv/p2p/com/client/P2PComClient.java
@@ -0,0 +1,101 @@
+package at.kv.p2p.com.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import at.kv.p2p.com.P2PMessage;
+
+public class P2PComClient {
+
+	private String serverAddress = "";
+	private int serverPort = 0;
+	
+	public P2PComClient(String serverAddress, int serverPort){
+		this.serverAddress = serverAddress;
+		this.serverPort = serverPort;
+	}
+	
+	/**
+	 * Sends a message to the server. 
+	 * @param msg response of the server
+	 * @return
+	 */
+	public P2PMessage send(P2PMessage msg){
+		
+		Socket socket = null;
+		
+		try{
+			socket = new Socket(serverAddress, serverPort);
+		}catch (IOException e){
+			System.err.println("Could not connect to server [" + serverAddress + ":" + serverPort + "]!");
+		}
+		
+		try {
+			return sendAndReceive(socket,msg);
+		} catch (IOException e) {
+			System.err.println("Handshake failed!");
+		}
+		
+		return null;
+		
+	}
+	
+	private P2PMessage sendAndReceive(Socket socket, P2PMessage requestMessage) throws IOException{
+		
+		InputStream is = socket.getInputStream();		
+		OutputStream os = socket.getOutputStream();
+		
+		os.write(requestMessage.toBytes());
+		os.flush();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		
+		String line = "";
+		P2PMessage msg = new P2PMessage();				
+		int payloadLength = -1;
+		while((line = br.readLine()) != null){
+			if(line.startsWith(P2PMessage.ENTRIE_CONTROL)){
+				msg.setControl(getEntrieValue(line));
+			}else if(line.startsWith(P2PMessage.ENTRIE_INFORMATION)){
+				msg.setInformation(getEntrieValue(line));
+			}else if(line.startsWith(P2PMessage.ENTRIE_PAYLOAD_LENGTH)){
+				payloadLength = Integer.parseInt(getEntrieValue(line));
+				break;
+			}
+		}
+		
+		
+		if(payloadLength != -1){
+			char[] payloadBuffer = new char[payloadLength];
+			
+			br.read(payloadBuffer,0, payloadBuffer.length);
+			byte[] payloadBufferByte = new byte[payloadLength];
+			for(int i = 0; i < payloadLength; i++){
+				payloadBufferByte[i] = (byte)payloadBuffer[i];
+			}
+			msg.setPayload(payloadBufferByte);
+		}		
+		
+		os.close();
+		is.close();
+		socket.close();		
+		return msg;
+	}
+	
+	private String getEntrieValue(String entrie){
+		
+		int sepPos = entrie.indexOf(P2PMessage.ENTRIE_SEPERATOR);
+		if(sepPos != -1){
+			return entrie.substring(sepPos+1);
+		}
+		return "";
+	}
+	
+	
+	
+	
+}
---
+++ p2p_communication/project/AS_P2P_PROJ_COM/src/at/kv/p2p/com/test/Main.java
@@ -0,0 +1,91 @@
+package at.kv.p2p.com.test;
+
+import at.kv.p2p.com.P2PMessage;
+import at.kv.p2p.com.client.P2PComClient;
+import at.kv.p2p.com.server.P2PComServer;
+import at.kv.p2p.com.server.P2PComServerControlCommand;
+import at.kv.p2p.com.server.P2PComServerCtrlCmdManager;
+
+public class Main {
+	
+	public Main(){	
+		
+	}
+	
+	public void startAsServer(){
+		P2PComServer server = new P2PComServer(3000);
+		System.out.println("Starting server!");
+		server.start();
+		
+		P2PComServerCtrlCmdManager ctrlManager = P2PComServerCtrlCmdManager.getInstance();
+		
+		ctrlManager.addControlCommand(new P2PComServerControlCommand("CTRL1"){
+
+			@Override
+			public void processRequest(P2PMessage request, P2PMessage response) {
+				System.out.println("Called [CTRL1]:");
+				System.out.println("Got payload -> " + new String(request.getPayload()));
+				
+				response.setControl("Server-CTRL1");
+				response.setInformation("Server-INFO");
+				response.setPayload("Server-PAYLOAD".getBytes());
+			}
+			
+		});
+		
+		ctrlManager.addControlCommand(new P2PComServerControlCommand("CTRL2"){
+
+			@Override
+			public void processRequest(P2PMessage request, P2PMessage response) {
+				System.out.println("Called [CTRL2]:");
+				System.out.println("Got payload -> " + new String(request.getPayload()));
+				
+				response.setControl("Server-CTRL2");
+				response.setInformation("Server-INFO");
+				response.setPayload("Server-PAYLOAD".getBytes());
+			}
+			
+		});
+		
+		
+	}
+	
+	public void startAsClient(){
+		P2PComClient client = new P2PComClient("localhost",3000);
+		
+		System.out.println("Sending CTRL1");
+		P2PMessage msg = new P2PMessage();
+		msg.setControl("CTRL1");
+		msg.setPayload("Client-PAYLOAD".getBytes());
+		P2PMessage response = client.send(msg);
+		System.out.println("Received:\n" + new String(response.toBytes()));
+		
+		
+		System.out.println("Sending CTRL2");
+		msg = new P2PMessage();
+		msg.setControl("CTRL2");
+		msg.setPayload("Client-PAYLOAD".getBytes());
+		response = client.send(msg);
+		System.out.println("Received:\n" + new String(response.toBytes()));
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("Arguments:");
+		System.out.println("-c ... starts as client");
+		System.out.println("-s ... starts as server");
+		
+		Main p2p = new Main();
+		
+		if(args[0].equals("-s")){
+			p2p.startAsServer();
+		}else if(args[0].equals("-c")){
+			p2p.startAsClient();
+		}
+		
+	}
+
+}
