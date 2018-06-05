e1417e88e6980f73a8e6ae883e86ddc3e277e71e
gitolafson <35873933+gitolafson@users.noreply.github.com>
2018-06-06 01:15:33 +0200

    seeder

---
+++ seeder/AS_P2P_PROJ_Seeder/src/at/kv/p2p/seeder/logic/P2PFile.java
@@ -0,0 +1,17 @@
+package at.kv.p2p.seeder.logic;
+
+public class P2PFile {
+	
+	private String ip;
+	private int port;
+	private int id;
+	private String name = "";
+	private int size = 0;
+	private String date = "";
+	
+	
+	public P2PFile(String ip, int port, int id){
+		
+	}
+
+}
---
+++ seeder/AS_P2P_PROJ_Seeder/src/at/kv/p2p/seeder/models/ViewModel.java
@@ -0,0 +1,15 @@
+package at.kv.p2p.seeder.models;
+
+import javafx.stage.Stage;
+
+public class ViewModel {
+	
+	protected Stage primaryStage;
+	
+	public ViewModel(Stage primaryStage){		
+		this.primaryStage = primaryStage;		
+	}
+	
+	
+
+}
