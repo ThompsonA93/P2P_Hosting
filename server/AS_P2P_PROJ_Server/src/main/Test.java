f0ecfe56223ce9c30af559c15fb3acb658dbb913
gitolafson <35873933+gitolafson@users.noreply.github.com>
2018-06-05 20:47:42 +0200

    Changed project dependencies

--- server/AS_P2P_PROJ_Server/src/main/Test.java
+++ server/AS_P2P_PROJ_Server/src/main/Test.java
@@ -1,9 +1,13 @@
 package main;
 
+import at.kv.p2p.com.client.P2PComClient;
+
 public class Test {
 	
 	public static void main(String[] args){
 		System.out.println("Hello world!");
+		
+		P2PComClient client = new P2PComClient("asdf",3000);
 	}
 	
 }
