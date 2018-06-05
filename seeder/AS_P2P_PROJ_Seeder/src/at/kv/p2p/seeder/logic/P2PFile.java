cb0d0232409213fee76258f1b795ae1b7b83ee41
gitolafson <35873933+gitolafson@users.noreply.github.com>
2018-06-06 01:25:15 +0200

    seeder

--- seeder/AS_P2P_PROJ_Seeder/src/at/kv/p2p/seeder/logic/P2PFile.java
+++ seeder/AS_P2P_PROJ_Seeder/src/at/kv/p2p/seeder/logic/P2PFile.java
@@ -10,8 +10,72 @@ public class P2PFile {
 	private String date = "";
 	
 	
-	public P2PFile(String ip, int port, int id){
-		
+	public P2PFile(String ip, int port, int size){
+		this.ip = ip;
+		this.port = port;
+		this.size = size;
 	}
+
+
+	public String getIp() {
+		return ip;
+	}
+
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+
+	public int getPort() {
+		return port;
+	}
+
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getSize() {
+		return size;
+	}
+
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+
+	public String getDate() {
+		return date;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
 
 }
