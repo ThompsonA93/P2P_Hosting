dfcec5316953bc536c120f8a772350e24c4e56f1
Bodawen <qwertyhotwen@gmail.com>
2018-06-05 21:20:36 +0200

    Server start

---
+++ server/AS_P2P_PROJ_Server/src/Server_Method/Servermethod.java
@@ -0,0 +1,130 @@
+package Server_Method;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import at.kv.p2p.com.P2PMessage;
+
+public class Servermethod {
+
+	private static Servermethod instance = null;
+	P2PMessage message;
+	static HashMap<String, List> GHTlist; // Adress:port as key, ressource list as value 
+	String command;
+	String information;
+	byte[] payload;
+	// Singleton all Information of Server will be stored here
+	public static Servermethod getInstance(){
+		if(instance == null){
+			instance = new Servermethod();
+		}
+		return instance;
+	}
+	public P2PMessage setmessage(P2PMessage message){
+		this.message = message;
+		this.command = message.getControl();
+		this.information = message.getInformation();
+		this.payload = message.getPayload();
+		return dealRequest();
+	}
+	public Servermethod(){
+		GHTlist = new HashMap<>();
+	}
+	/**
+	 * check Command, and determine which method should be called
+	 * 
+	 * @return result Response P2PMessage to Client
+	 */
+	public P2PMessage dealRequest(){
+		P2PMessage result = new P2PMessage();
+		switch (command) {
+		case "AddRessource":
+			result =  updateDHT();
+			break;
+		case "GetRessource":
+			result =  fetchFromDHT();
+			break;
+		case "detachRessource":
+			result =  updateDHT();
+			break;
+		case "listRessources":
+			result =  listDHTRessouces();
+			break;
+		}
+		return result;
+	}
+	public P2PMessage fetchFromDHT(){
+		return null;
+	}
+	/**
+	 * Get Ressource list from Client, store those list as value in a Hashmap, which key is "IP@Port".
+	 * 
+	 * @return result Response P2PMessage to Client
+	 */
+	public P2PMessage updateDHT(){
+		String filenames = new String(payload);
+		List<String> list = new ArrayList<>();
+		String[] filenamelist = filenames.split("\n");
+		System.out.println("filename:");
+		for (String string : filenamelist) {
+			list.add(string);
+		}
+		GHTlist.put(information, list);
+		Set<String> setKey = GHTlist.keySet();
+		Iterator<String> iterator = setKey.iterator();
+		while(iterator.hasNext()){
+			String key = iterator.next();
+			System.out.println("adress = "+key);
+			List<String> flist = new ArrayList<>();
+			flist = GHTlist.get(key);
+			for (String string : flist) {
+				System.out.println(string);
+			}
+		}
+		P2PMessage response = new P2PMessage();
+		response.setControl("Server response");
+		response.setInformation("AddRessource is successful");
+		response.setPayload("ServerPAYLOAD".getBytes());
+		return response;
+	}
+	
+	public P2PMessage detachDHT(){
+		
+		
+		
+		return null;
+	}
+	public P2PMessage listDHTRessouces(){
+		return null;
+	}
+	
+	public String getCommand() {
+		return command;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public String getInformation() {
+		return information;
+	}
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+
+	public byte[] getPayload() {
+		return payload;
+	}
+
+	public void setPayload(byte[] payload) {
+		this.payload = payload;
+	}
+	
+}
--- server/AS_P2P_PROJ_Server/src/main/Test.java
+++ server/AS_P2P_PROJ_Server/src/main/Test.java
@@ -1,13 +1,98 @@
 package main;
 
-import at.kv.p2p.com.client.P2PComClient;
+import java.util.ArrayList;
+import java.util.List;
+
+import Server_Method.Servermethod;
+import at.kv.p2p.com.P2PMessage;
+ import at.kv.p2p.com.client.P2PComClient;
+ import at.kv.p2p.com.server.P2PComServer;
+ import at.kv.p2p.com.server.P2PComServerControlCommand;
+ import at.kv.p2p.com.server.P2PComServerCtrlCmdManager;
+import at.kv.p2p.com.test.Main;
+
+ 
 
 public class Test {
 	
-	public static void main(String[] args){
-		System.out.println("Hello world!");
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Arguments:");
+		System.out.println("-c ... starts as client");
+		System.out.println("-s ... starts as server");
 		
-		P2PComClient client = new P2PComClient("asdf",3000);
+		Test p2p = new Test();
+		
+		if(args[0].equals("-s")){
+			p2p.startAsServer();
+		}else if(args[0].equals("-c")){
+			p2p.startAsClient();
+		}
+		
 	}
+	
+	public void startAsServer(){
+		P2PComServer server = new P2PComServer(3000);
+ 		System.out.println("Starting server!");
+ 		server.start();
+ 		
+ 		P2PComServerCtrlCmdManager ctrlManager = P2PComServerCtrlCmdManager.getInstance();
+ 			ctrlManager.addControlCommand(new P2PComServerControlCommand("AddRessource"){
+ 			@Override
+ 			public void processRequest(P2PMessage request, P2PMessage response) {
+ 				
+				System.out.println("Called [AddRessource]:");
+ 				System.out.println("Got payload -> " + new String(request.getPayload()));
+ 				P2PMessage servermassage = Servermethod.getInstance().setmessage(request);
+ 				response.setControl(servermassage.getControl());
+ 				response.setInformation(servermassage.getInformation());
+ 				response.setPayload(servermassage.getPayload());
+ 			}
+ 			
+ 		});
+ 		
+ 		ctrlManager.addControlCommand(new P2PComServerControlCommand("CTRL2"){
+ 
+ 			@Override
+ 			public void processRequest(P2PMessage request, P2PMessage response) {
+ 				System.out.println("Called [CTRL2]:");
+ 				System.out.println("Got payload -> " + new String(request.getPayload()));
+ 				
+ 				response.setControl("Server-CTRL2");
+ 				response.setInformation("Server-INFO");
+ 				response.setPayload("Server-PAYLOAD".getBytes());
+ 			}
+ 			
+ 		});
+	}
+	
+	public void startAsClient(){
+		System.out.println("Hello");
+		P2PComClient client = new P2PComClient("localhost",3000);
+		
+		System.out.println("Sending AddRessource");
+		P2PMessage msg = new P2PMessage();
+		msg.setControl("AddRessource");
+		msg.setInformation("127.0.0.1@3534");
+		String pa = "mypicture.jpg\nmytext.tex";
+		msg.setPayload(pa.getBytes());
+		P2PMessage response = client.send(msg);
+		
+		if(response != null){
+			System.out.println("Received:\n" + new String(response.toBytes()));
+			
+			
+			System.out.println("Sending CTRL2");
+			msg = new P2PMessage();
+			msg.setControl("CTRL2");
+			msg.setPayload("Client-PAYLOAD".getBytes());
+			response = client.send(msg);
+			System.out.println("Received:\n" + new String(response.toBytes()));
+		}else{
+			System.out.println("Connection failed!");
+		}
+		
+	}
+	
 	
 }
